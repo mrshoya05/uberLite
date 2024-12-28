3231319c057b8866823418059058ed13c3a2f203
shoyabkhan <shoyabkhan0508@gmail.com>
2024-12-28 21:17:33 +0530

    update

---
+++ frontend/src/pages/Start.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Start = () => {
+  return (
+    <>
+    
+    
+    
+    </>
+  )
+}
+
+export default Start
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
+import autoprefixer from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
